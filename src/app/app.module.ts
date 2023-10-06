303222dfac5cb28900212e31aa3ef50b46fc8bf0
Wellington Delmondes <wellington.apk@gmail.com>
2023-10-05 22:24:58 -0300

    Diretivas ngFor e ngIf.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FirstComponent } from './component/first/first.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FirstComponent,
   ],
   imports: [
     BrowserModule
